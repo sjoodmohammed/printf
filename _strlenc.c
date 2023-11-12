af4543ff29cbb28470d8fb74f7b8ccb160c411ef
Abuabaida3 <abuabaida.ahmed@gmail.com>
2023-11-12 10:50:05 -0800

    my commit

---
+++ _strlenc.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlenc - function that str len.
+ * str: the str to get its value
+ * return: i
+ */
+int _strlenc(const char * str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+		return (i)
+}
---
+++ print_percent.c
@@ -0,0 +1,6 @@
+#include "main.h"
+ in print_percent(void)
+{_putchar(37);
+	return (0);
+}
+
