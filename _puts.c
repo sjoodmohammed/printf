6f5914edf7415dc848bf5ba1e6fdbceb3c370c68
sjoodmohammed <sjoodmohammedabdalla@gmail.com>
2023-11-15 22:09:55 -0800

    printf

--- _puts.c
+++ _puts.c
@@ -21,7 +21,7 @@ return (str - a);
 int _putchar(int c)
 {
 static int i;
-static char buf (OUTPUT_BUF_SIZE);
+static char buf [OUTPUT_BUF_SIZE];
 if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
 {
 write (1, buf, i);
